f87027452940a34623998d590f209f009992c5f8
abshaibu <shaibuabdulsalam56@gmail.com>
2022-06-06 23:53:44 +0100

    changed inputs to semantic format

--- app.js
+++ app.js
@@ -1,31 +1,45 @@
 const username = document.querySelector('.username');
 const logoutBtn = document.querySelector('.logout-btn');
-const startBtn = document.querySelector('.start-btn');
+const usernameForm = document.querySelector('.username-form');
+const textForm = document.querySelector('.text-form');
 const initials = document.querySelector('.initials');
 const chatWrapper = document.querySelector('.chat-wrapper');
+const chatBox = document.querySelector('.chat-box');
 const chatHomepage = document.querySelector('.homepage');
 const errorMessage = document.querySelector('.error-message');
-const chatInput = document.querySelector('.chat-input').value;
+const chatInput = document.querySelector('.chat-input');
 const usernameInput = document.querySelector('.username-input');
 
+// Sending messages
+textForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const p = document.createElement('p');
+    p.classList.add('message-text');
+    console.log(chatInput.value);
+    p.innerHTML = chatInput.value;
+    console.log(p);
+    chatBox.appendChild(p);
+    chatInput.value = '';
+})
+
 // Getting started
-startBtn.addEventListener('click', () => {
-    console.log('clicked');
-    console.log(usernameInput);
-    if (usernameInput.innerHTML === '') {
+usernameForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if (usernameInput.value === '') {
         errorMessage.style.display = 'block';
     } else {
-        username.innerHTML = usernameInput.trim();
-        console.log('clicked');
-        console.log(usernameInput);
+        username.innerHTML = usernameInput.value;
         errorMessage.style.display = 'none';
         chatHomepage.style.display = 'none';
         chatWrapper.style.display = 'block';
-        initials.innerHTML = username.innerHTML[0];
+        initials.innerHTML = username.innerHTML[0].toUpperCase();
+        usernameInput.value = '';
     }
 });
 
+// Logging out
 logoutBtn.addEventListener('click', () => {
+    chatBox.innerHTML = '';
     chatWrapper.style.display = 'none';
     chatHomepage.style.display = 'block';
 })
